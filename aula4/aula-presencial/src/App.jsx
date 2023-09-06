7348e8f36611cb5a6a14eea5b23aa316eb773fdf
artistrea <apiravesi@gmail.com>
2023-09-06 11:08:19 -0300

    sla

--- aula4/aula-presencial/src/App.jsx
+++ aula4/aula-presencial/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { api } from "./utils/api";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function validate() {
+  return true;
 }
 
-export default App
+function App() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  function handleSubmit(e) {
+    e.preventDefault();
+    const isValid = validate();
+
+    // chamar api
+    api.post("users/login");
+  }
+
+  function handleChange(key, value) {
+    setUser((usr) => ({ ...usr, [key]: value }));
+  }
+
+  return (
+    <main>
+      <pre>
+        <code>{JSON.stringify(user, null, 2)}</code>
+      </pre>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="inp2">Email:</label>
+        <input
+          className="text-black"
+          type="text"
+          id="inp2"
+          value={user.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+        <br />
+        <br />
+        <br />
+        <label htmlFor="inp3">Senha:</label>
+        <input
+          className="text-black"
+          type="password"
+          id="inp3"
+          value={user.password}
+          onChange={(e) => handleChange("password", e.target.value)}
+        />
+        <br />
+        <br />
+        <button className="py-2 px-3 bg-white text-black rounded">Login</button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
